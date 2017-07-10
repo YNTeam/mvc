47a120e624eb06e0072f21dbc3a2165eca33202b
pengyour <hansyou@vip.qq.com>
2017-07-10 16:36:08 +0800

    first commit

---
+++ WinFormsApp/LoginForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Model;
+
+namespace WindowsFormsApplication1
+{
+    public partial class LoginForm : Form
+    {
+        string MyConnString = "Server=localhost;Database=sampledb;uid=root;Pwd=123456";
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 记住密码_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void summit_Click(object sender, EventArgs e)
+        {
+            //创建连接
+            string name = txtName.Text;
+            string pwd = txtPwd.Text;
+            if (new BLL.LoginService().QueryUserIsExist(name, pwd))
+            {
+                Console.WriteLine("登录成功！");
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+                this.Hide();
+            }
+            else {
+                Console.WriteLine("用户名或密码错误，登录失败！");
+                //                     MessageBox.Show("用户名或密码错误，登录失败！", "提示消息");
+                Labelerrormsg.Text = "用户名或密码错误，登录失败！";
+            }
+        }
+
+        private void txtPwd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("真的要退出程序吗？", "退出程序", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Contacts contacts = new Contacts();
+            contacts.Show();
+        }
+    }
+}
